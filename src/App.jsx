87cdd66c28ea6541e27d0cd4dd773857db65a1d2
hishmat-dev <developerhishmat1@gmail.com>
2025-05-18 11:47:14 +0500

    set update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
 import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
+import LoadingComponent from './components/ui/LoadingComponent.jsx'; 
 
 const Landing = lazy(() => import('./Landing.jsx'));
 const ForgotPassword = lazy(() => import('./components/Forgot/ForgotPassword.jsx'));
 const Register = lazy(() => import('./components/Register/Register.jsx'));
-const LoadingComponent = lazy(() => import('./components/ui/LoadingComponent.jsx'));
 
 function App() {
   return (
-    <Router>
-      <Suspense fallback={<LoadingComponent/>}>
+    <BrowserRouter
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+    >
+      <Suspense fallback={<LoadingComponent />}>
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/register" element={<Register />} />
         </Routes>
       </Suspense>
-    </Router>
+    </BrowserRouter>
   );
 }
 
